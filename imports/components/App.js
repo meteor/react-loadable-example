461db2c7edb7a93f5d83894d5a63ef0a5c253bc6
David Burles <dburles@gmail.com>
2017-04-11 19:12:31 +1000

    update to react-loadable 3

--- imports/components/App.js
+++ imports/components/App.js
@@ -4,20 +4,18 @@ import Loading from './Loading';
 import fakeDelay from './fakeDelay';
 import path from 'path';
 
-let LoadableExample = Loadable(
-  () => fakeDelay(400).then(
-    () => import('./Example')
-  ).then(ex => ex.default),
-  Loading,
-  null,
-  path.resolve(__dirname, './Example')
-);
+const LoadableExample = Loadable({
+  loader: () =>
+    fakeDelay(400).then(() => import('./Example')).then(ex => ex.default),
+  LoadingComponent: Loading,
+  serverSideRequirePath: path.resolve(__dirname, './Example')
+});
 
 export default function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <LoadableExample/>
+      <LoadableExample />
     </div>
   );
 }
